012a742e0cb48ad2192dd5621365a17ed214f20e
choomba <karyagin.finworks@gmail.com>
2021-06-14 17:24:47 +0300

    adding inital script

---
+++ ratios.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+
+calls_df = pd.read_csv ("~/options_analysis/TQQQcalls_subset.csv")
+puts_df = pd.read_csv ("~/options_analysis/TQQQputs_subset.csv")
+
+print (calls_df)
+
+
+#total_call_oi = 
+#total_put_oi = 
